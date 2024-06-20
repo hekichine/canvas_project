fc92ecf2b13a9e0c37f328c5f6ab070c2fdf9e01
hekichien <chienlx1005@gmail.com>
2024-06-20 17:19:58 +0700

    update fireworks animation canvas

--- canvas-core.js
+++ canvas-core.js
@@ -327,48 +327,41 @@ class GalacticLight extends HTMLElement {
 }
 customElements.define("galactic-light", GalacticLight);
 
-// 
+//
 // =================================
 //
 //      FIRE WORKS
 //
 // =================================
-class Fire {
-  constructor(x, y, radius, color) {
+class fireWork_Particle {
+  constructor(c, x, y, firework, color) {
     this.c = c;
     this.x = x;
     this.y = y;
-    this.radius = radius;
+    this.lifeSpan = this.random(1000, 50);
+    this.firework = firework;
     this.color = color;
-  }
-  draw() {
-    this.c.beginPath();
-    this.c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    this.c.fillStyle = this.color;
-    this.c.fill();
-    this.c.closePath();
-  }
-  update() {
-    this.draw();
-  }
-}
-class FireWorks extends HTMLElement {
-  constructor() {
-    super();
-    this.canvas = this.querySelector("canvas");
-    this.c = this.canvas.getContext("2d");
-    this.canvas.width = innerWidth;
-    this.canvas.height = innerHeight;
-    this.mouse = {
-      x: innerWidth / 2,
-      y: innerHeight / 2,
-    };
+    this.particleRadiusRange = { max: 3, min: 1 };
+    this.explosionRadiusRange = { max: 5, min: -5 };
+    this.fireworkRandomXPathRange = { max: 2, min: -2 };
+    this.radius = this.random(
+      this.particleRadiusRange.max,
+      this.particleRadiusRange.min
+    );
+    this.history = [];
+    this.gravity = 0.005;
+    this.tail = 5;
+    this.fireworkOnXaxis = 2;
+    this.fireworkGravity = 0.2;
+    this.particlesGravity = 0.08;
+    this.fireworkRadius = 4;
+    this.particlesLifespan = 10;
     this.colors = [
-      "#2185c",
-      "#7ecefd",
-      "#fff6e5",
-      "#ff7f66",
-      "#fff",
+      "#ffffff",
+      "#00ffff",
+      "#d3ffce",
+      "#f0f8ff",
+      "#faebd7",
       "#add8e6",
       "#00008b",
       "#84e4e0",
@@ -385,31 +378,235 @@ class FireWorks extends HTMLElement {
       "#c8ad7f",
       "#b87333",
       "#dadbdd",
-      "",
     ];
-    this.objects = [];
 
-    window.addEventListener("resize", () => {
+    if (this.firework) {
+      this.vx = this.random(this.fireworkOnXaxis, -this.fireworkOnXaxis);
+      this.vy = this.random(-9, -15);
+      this.gravity = this.fireworkGravity;
+    } else {
+      this.vy = this.random(
+        this.explosionRadiusRange.max,
+        this.explosionRadiusRange.min
+      ); // firework radius on Y axis
+      this.vx = this.random(
+        this.explosionRadiusRange.max,
+        this.explosionRadiusRange.min
+      ); //firework radius on X axis
+      this.vy = this.vy * this.random(3, 2);
+      this.vx = this.vx * this.random(3, 2);
+      this.gravity = this.particlesGravity;
+      if (this.random(600, 1) < 200) {
+        this.vx *= this.random(2, 1);
+        this.vy *= this.random(2, 1);
+      }
+    }
+  }
+  done() {
+    if (this.lifeSpan < 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  draw() {
+    this.c.fillStyle = this.color;
+
+    this.c.beginPath();
+
+    this.c.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    this.c.fill();
+
+    for (let i = 0; i < this.history.length - 1; i++) {
+      this.c.beginPath();
+      this.c.moveTo(this.history[i].x, this.history[i].y);
+      this.c.lineTo(this.history[i + 1].x, this.history[i + 1].y);
+      this.c.strokeStyle = this.colors[Math.floor(Math.random()*this.colors.length)];
+      this.c.lineWidth = this.radius;
+      this.c.stroke();
+      this.c.closePath();
+    }
+  }
+  update() {
+    this.y += this.vy;
+    this.x += this.vx;
+    this.vy += this.gravity;
+    if (this.firework) {
+      this.x += this.random(
+        this.fireworkRandomXPathRange.max,
+        this.fireworkRandomXPathRange.min
+      ); //Randomize the direction on X axis
+      this.radius = this.random(
+        this.fireworkRadius,
+        this.fireworkRadius - this.fireworkRadius / 2
+      );
+    }
+    if (!this.firework) {
+      this.vy *= 0.9;
+      this.vx *= 0.9;
+      this.lifeSpan -= this.particlesLifespan;
+
+      // this.vx += random(.5,-.5);
+      // this.vy += random(.5,-.5);
+      if (this.lifeSpan < 0) {
+        this.done();
+      }
+    }
+
+    this.history.push({
+      x: this.x,
+      y: this.y,
+    });
+
+    if (this.history.length > this.tail) {
+      this.history.shift();
+    }
+  }
+  random(max, min) {
+    let x = Math.random() * (max - min) + min;
+    return x;
+  }
+}
+class FireWork {
+  constructor(canvas, c, color) {
+    this.numberOfParticles = 90;
+    this.canvas = canvas;
+    this.c = c;
+    this.color = color;
+
+    this.firework = new fireWork_Particle(
+      this.c,
+      this.random(this.canvas.width / 2 - 50, this.canvas.width / 2),
+      this.canvas.height,
+      true,
+      this.color
+    );
+    this.exploded = false;
+    this.particles = [];
+  }
+  done() {
+    if (this.exploded && this.particles.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  explode() {
+    for (let i = 0; i < this.numberOfParticles; i++) {
+      this.particles.push(
+        new fireWork_Particle(
+          this.c,
+          this.firework.x,
+          this.firework.y,
+          false,
+          this.color
+        )
+      );
+    }
+  }
+  update() {
+    if (!this.exploded) {
+      this.firework.draw();
+      this.firework.update();
+
+      if (this.firework.vy > 0) {
+        this.firework.vy = 0;
+        this.exploded = true;
+        this.explode();
+      }
+    }
+
+    for (let i = this.particles.length - 1; i >= 0; i--) {
+      this.particles[i].draw();
+      this.particles[i].update();
+      if (this.particles[i].done()) {
+        this.particles.splice(i, 1);
+        //this.done();
+      }
+    }
+  }
+  random(max, min) {
+    let x = Math.random() * (max - min) + min;
+    return x;
+  }
+}
+class FireWorks extends HTMLElement {
+  constructor() {
+    super();
+    this.canvas = this.querySelector("canvas");
+    this.c = this.canvas.getContext("2d");
+    this.canvas.width = innerWidth;
+    this.canvas.height = innerHeight;
+    this.mouse = {
+      x: innerWidth / 2,
+      y: innerHeight / 2,
+    };
+    this.colors = [
+      "#ffffff",
+      "#00ffff",
+      "#d3ffce",
+      "#f0f8ff",
+      "#faebd7",
+      "#add8e6",
+      "#00008b",
+      "#84e4e0",
+      "#ece5b6",
+      "#e1eb78",
+      "#caffd6",
+      "#FFFDD0",
+      "#ffdf00",
+      "#001f3f",
+      "#fcc6de",
+      "#ffa500",
+      "#d966d9",
+      "#b1907f",
+      "#c8ad7f",
+      "#b87333",
+      "#dadbdd",
+    ];
+    this.density = 2;
+    this.fireworksArrayLength = 50;
+    this.fireworks = [];
+    
+    window.addEventListener('resize', () => {
       this.canvas.width = innerWidth;
       this.canvas.height = innerHeight;
       this.init();
     });
-    this.init();
     this.animate();
   }
-  init() {
-    for (let i = 0; i < 400; i++) {
-      // objects.push();
+
+  animate() {
+    requestAnimationFrame(() => {
+      this.animate();
+    });
+    this.c.fillStyle = "rgba(0,0,0,.3)";
+    this.c.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+    if (Math.round(this.random(0, 100)) < this.density) {
+      this.fireworks.push(
+        new FireWork(
+          this.canvas,
+          this.c,
+          this.colors[Math.floor(Math.random() * this.colors.length)]
+        )
+      );
+      // fireworks.push(new Firework());
+    }
+
+    for (let i = this.fireworks.length - 1; i >= 0; i--) {
+      this.fireworks[i].update();
+      if (this.fireworks[i].done()) {
+      }
+    }
+
+    if (this.fireworks.length >= this.fireworksArrayLength) {
+      this.fireworks.splice(0, 1);
     }
   }
-  animate() {
-    requestAnimationFrame(this.animate.bind(this));
-    this.c.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.c.fillText("HTML CANVAS", this.mouse.x, this.mouse.y);
-
-    // this.objects.forEach(object => {
-    //   object.update();
-    // })
+  random(max, min) {
+    let x = Math.random() * (max - min) + min;
+    return x;
   }
 }
-customElements.define('fire-works',FireWorks)
+customElements.define("fire-works", FireWorks);
